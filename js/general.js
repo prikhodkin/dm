3c1355ed6546562f02c7aba9c29cc88338cd9e34
prikhodkin <info@prikhodkin.com>
2020-04-29 02:58:19 +0500

    Updates

--- js/general.js
+++ js/general.js
@@ -4,4 +4,15 @@ $('.menu__btn').click(function () {
   $('.menu__hamburger').toggleClass('menu__hamburger--active');
   $('.menu__list').slideToggle();
 });
+
+if ($(window).width() < 1320) {
+  $('.service__list').slick({
+    nextArrow: ".service__arrow--right",
+    prevArrow: ".service__arrow--left"
+  });
+  $('.count__small').html($(".service__list").slick("getSlick").slideCount);
+  $(".service__list").on("afterChange", function (event, slick, currentSlide, nextSlide) {
+    $(".count__big").html(currentSlide + 1);
+  });
+}
 //# sourceMappingURL=maps/general.js.map
