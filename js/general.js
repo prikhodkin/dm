f33b33affcc67a0a93d12807fcd31984901ec650
prikhodkin <info@prikhodkin.com>
2020-05-03 02:23:55 +0500

    Updates

--- js/general.js
+++ js/general.js
@@ -5,14 +5,33 @@ $('.menu__btn').click(function () {
   $('.menu__list').slideToggle();
 });
 
-if ($(window).width() < 1320) {
+if ($(window).width() < 1260) {
   $('.service__list').slick({
     nextArrow: ".service__arrow--right",
     prevArrow: ".service__arrow--left"
   });
-  $('.count__small').html($(".service__list").slick("getSlick").slideCount);
+  $('.service__control .count__small').html($(".service__list").slick("getSlick").slideCount);
   $(".service__list").on("afterChange", function (event, slick, currentSlide, nextSlide) {
-    $(".count__big").html(currentSlide + 1);
+    $(".service__control .count__big").html(currentSlide + 1);
   });
 }
+
+$('.case__list').slick({
+  nextArrow: ".case__arrow--right",
+  prevArrow: ".case__arrow--left",
+  fade: true
+});
+$('.case__control .count__small').html($(".case__list").slick("getSlick").slideCount);
+$(".case__list").on("afterChange", function (event, slick, currentSlide, nextSlide) {
+  $(".case__control .count__big").html(currentSlide + 1);
+});
+$('.stages__list').slick({
+  nextArrow: ".stages__arrow--right",
+  prevArrow: ".stages__arrow--left",
+  fade: true
+});
+$('.stages .count__small').html($(".stages__list").slick("getSlick").slideCount);
+$(".stages__list").on("afterChange", function (event, slick, currentSlide, nextSlide) {
+  $(".stages .count__big").html(currentSlide + 1);
+});
 //# sourceMappingURL=maps/general.js.map
